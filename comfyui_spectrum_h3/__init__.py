d13cb743024750513e33914744e1274ca706d158
xmarre <54859656+xmarre@users.noreply.github.com>
2026-08-13 07:18:27 +0200

    Install replay generic-correction gate

--- comfyui_spectrum_h3/__init__.py
+++ comfyui_spectrum_h3/__init__.py
@@ -5,6 +5,7 @@ from .generic_correction import install_generic_residual_correction
 from .minimax_h3 import locate_minimax_h3_inner, require_native_minimax_h3
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 from .replay_component_shadow import install_replay_component_decomposition
+from .replay_generic_correction_gate import install_replay_generic_correction_gate
 from .replay_shadow_composition import install_replay_shadow_composition
 from .replay_trust_shadow import install_replay_native_trust_shadow
 from .runtime import SpectrumH3Runtime
@@ -15,6 +16,7 @@ install_forecast_trust_probe()
 install_replay_native_trust_shadow()
 install_replay_component_decomposition()
 install_replay_shadow_composition()
+install_replay_generic_correction_gate()
 install_er_sde_tail_policy()
 
 __all__ = [
